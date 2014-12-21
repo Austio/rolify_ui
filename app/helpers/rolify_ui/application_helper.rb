a3446339db9844d19aaabfb078bc282470c21f3a
Austio@windowslaptop <lonnieastory@gmail.com>
2014-12-21 10:53:22 -0600

    Updated readme and moved methods around in helper

--- app/helpers/rolify_ui/application_helper.rb
+++ app/helpers/rolify_ui/application_helper.rb
@@ -7,8 +7,30 @@ module RolifyUi
     #TODO - Add other normal attributes for what objects with roles will be identified by (name, first_name, last_name, etc)
     #  we are displaying the way that the user would want this to look
     #  they can either add a rolified_name class to their class or we try to guess
-    def object_human_name(obj)
-      obj.try(:rolified_name) || obj.try(:name) || obj.try(:email) || obj.try(:fname) || obj.try(:organization) || obj.id
+    def rolified_object_name(obj)
+      obj.try(:rolified_name) || obj.try(:name) || obj.try(:email) || obj.id
+    end
+
+
+    def display_role(role)
+      if role.resource_id
+        display_instance_role(role)
+      else
+        display_global_role(role)
+      end
+    end
+
+    def display_global_role(role)
+      role_content = content_tag(:p, role.name)
+      content_tag(:div, role_content, class: "global_role")
+    end
+
+    def display_instance_role(role)
+      'Instance'+ role.name + '-' + role.resource.inspect
+    end
+
+    def link_to_rolified_class(klass)
+      link_to klass.name, "/admin/roles/rolified_classes?klass=#{klass.name}", :class => 'search-link'
     end
   end
 end
--- app/helpers/rolify_ui/rolified_objects.rb
+++
@@ -1,8 +0,0 @@
-module RolifyUi
-  module RolifiedObjectsHelper
-    def link_to_rolified_class(klass)
-      link_to klass.name, "/admin/roles/rolified_classes?klass=#{klass.name}", :class => 'search-link'
-    end
-
-  end
-end
